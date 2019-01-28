7781f9e205e1ecea7eee4875961866fd4063415c
Udo Walter <udo.walter@mars.uni-freiburg.de>
2019-01-28 17:00:20 +0100

    add midi_marker_selection

---
+++ marker_selection/midi_marker_selection.py
@@ -0,0 +1,25 @@
+import cv2
+import mido
+from marker_selection import MarkerSelection
+
+
+class MidiMarkerSelection(MarkerSelection):
+
+    def __init__(self, ip, port):
+        self.midi_port = mido.open_output()
+        self.marker_state = {}
+    
+    def blink_action(self, marker_id):
+        if marker_id == -1: return
+        toggle = self.marker_state.get(marker_id, False)
+        print("Blink: Marker " + str(marker_id) + " " + str(not toggle))
+        self.marker_state[marker_id] = not toggle
+        midi_port.send(mido.Message('control_change', channel=0, control=marker_id, value=127 if toggle else 0))  
+
+    def overlay_marker(self, marker_id, marker_corners, frame):
+        if self.marker_state.get(marker_id, False):
+            cv2.polylines(frame, marker_corners, True, (0, 0, 255), 2)
+
+
+if __name__ == '__main__':
+    MidiMarkerSelection().run()
